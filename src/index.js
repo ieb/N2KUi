58e667549376562156568b6798b4c0856590ae0a
Ian Boston <ieb@tfd.co.uk>
2024-06-22 11:57:16 +0100

    Fixed no display of graphs initially

--- src/index.js
+++ src/index.js
@@ -6,7 +6,8 @@ const html = htm.bind(h);
 if ('serviceWorker' in navigator) {
   navigator.serviceWorker.register('/n2k/worker.js', { scope: '/n2k/', type: 'module' });
   navigator.serviceWorker.ready.then((registration) => {
-    registration.active.postMessage({ cacheEnabled: false });
+    const cacheEnabled = !window.location.hash.includes('disableCache');
+    registration.active.postMessage({ cacheEnabled });
   });
 }
 
